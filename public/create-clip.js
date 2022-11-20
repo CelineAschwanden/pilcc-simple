45101a6f80613d47a544105fe8ca1fbc95baa73b
Ciliscu <ciliscu@pm.me>
2022-11-20 03:24:04 +0100

    create qr code after clip creation

--- public/create-clip.js
+++ public/create-clip.js
@@ -1,30 +1,35 @@
 
 const errorMessage = document.getElementById('error-message');
 const errorMessageText = document.getElementById('error-message-text');
-
-//set current url to show on success
+const currentUrl = window.location.href;
 
 function createClip() {
     const xhr = new XMLHttpRequest();
-    const url = window.location.href;
-    
     const content = document.getElementById('clip-content').value;
     const lifetime = document.getElementById('clip-lifetime').value;
     const data = JSON.stringify({content: content, lifetime: lifetime});
 
     //request settings
-    xhr.open("POST", url, true);
+    xhr.open("POST", currentUrl, true);
     xhr.setRequestHeader('Content-Type', 'application/json');
 
     //result callback
     xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4 && xhr.status === 200) {
+        if (xhr.readyState === 4 && xhr.status === 200) { //OK
             document.getElementById('clip-form').style.display = 'none';
-
-        } else if (xhr.readyState === 4 && xhr.status === 400) {
+            document.getElementById('clip-success').style.display = 'block';
+            const qrcode = new QRCode(document.getElementById('clip-qrcode'), {
+                text: currentUrl,
+                width: 150,
+                height: 150,
+                colorDark : "#000000",
+	            colorLight : "#ffffff",
+                correctLevel: QRCode.CorrectLevel.H
+            });
+        } else if (xhr.readyState === 4 && xhr.status === 400) { //Invalid input
             errorMessage.style.display = 'flex';
             errorMessageText.textContent = 'Invalid input';
-        } else if (xhr.readyState === 4 && xhr.status === 409) {
+        } else if (xhr.readyState === 4 && xhr.status === 409) { //Conflict
             errorMessage.style.display = 'flex';
             errorMessageText.textContent = 'A clip with this ID already exists!';
         }
