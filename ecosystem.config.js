eafaaeff6e01dd682fd288ebfff434f789bdc709
Celine Aschwanden <Celine_Aschwanden@sluz.ch>
2022-12-02 16:20:32 +0100

    changed path and added key in pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,21 +2,16 @@ module.exports = {
   apps : [{
     name: "Pilcc",
     script: './index.js',
-    env: {
-      NODE_ENV: "development"
-    },
-    env_production: {
-      NODE_ENV: "production"
-    }
   }],
 
   deploy : {
     production : {
+      key  : '../../.ssh/id_rsa.pub',
       user : 'root',
       host : '178.128.245.25',
       ref  : 'origin/master',
       repo : 'git@github.com:CelineAschwanden/pilcc.git',
-      path : '/pilcc',
+      path : '/root/pilcc',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
