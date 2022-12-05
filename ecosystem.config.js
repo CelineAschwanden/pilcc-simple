1b8cba91cdbf3a80d49a542bf33168d1f6810ae4
Celine Aschwanden <Celine_Aschwanden@sluz.ch>
2022-12-05 15:05:22 +0100

    changed pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,25 +2,21 @@ module.exports = {
   apps : [{
     name: "Pilcc",
     script: './index.js',
-    env: {
-      NODE_ENV: "development",
-      PATH: process.env.PATH
-    },
     env_production: {
-      NODE_ENV: "production",
-      PATH: process.env.PATH
+      "PORT": 3000
     }
   }],
 
   deploy : {
     production : {
-      key  : '../../.ssh/id_rsa.pub',
+      key  : '../../.ssh/id_rsa',
       user : 'root',
       host : '178.128.245.25',
       ref  : 'origin/master',
       repo : 'git@github.com:CelineAschwanden/pilcc.git',
-      path : '/root/pilcc',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      path : '/var/www/Pilcc',
+      'post-setup': 'yarn install',
+      'post-deploy' : 'pm2 reload ecosystem.config.js --env production'
     }
   }
 };
