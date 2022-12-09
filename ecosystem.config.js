395f7fc77b9b9339c6dfff891219d4d44b5f833d
Celine Aschwanden <Celine_Aschwanden@sluz.ch>
2022-12-09 11:25:20 +0100

    changed key path in pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
 
   deploy : {
     production : {
-      key  : '../../.ssh/id_rsa',
+      key  : './github_action_key',
       user : 'root',
       host : '178.128.245.25',
       ref  : 'origin/master',
