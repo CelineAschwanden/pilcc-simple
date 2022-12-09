36c492cd3c64132696d1dd68f643284536011f48
Celine Aschwanden <Celine_Aschwanden@sluz.ch>
2022-12-09 14:38:07 +0100

    fixed typo in github action file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
 
   deploy : {
     production : {
-      key  : '~/.ssh/github_action_key',
+      key  : './github_action_key',
       user : 'root',
       host : '178.128.245.25',
       ref  : 'origin/master',
